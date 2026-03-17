09a7dbb18a3528708202e6c5359b2bb9b1c6e954
Artem <nadulich2019@mail.ru>
2026-03-17 12:37:07 +0300

    Revert "Added serialization"
    This reverts commit ea7283591d62d8b02efc81cc22cd0c386ebc13e1.
    
    # Conflicts:
    #	Assets/Scripts/Editor/EditorSerializer.cs
    #	Assets/Scripts/ThirdPersonGame/EntryPoint.cs
    #	Assets/Scripts/ThirdPersonGame/Storage/Data/UIData.cs
    #	Assets/Scripts/ThirdPersonGame/Storage/Data/UIElementData.cs
    #	Assets/Scripts/ThirdPersonGame/Storage/Storage.cs
    #	Assets/Scripts/ThirdPersonGame/UI/UICreator.cs
    #	Assets/StreamingAssets/data/UI.save

--- Assets/Scripts/ThirdPersonGame/EntryPoint.cs
+++ Assets/Scripts/ThirdPersonGame/EntryPoint.cs
@@ -1,14 +1,10 @@
-using Assets.Scripts.ThirdPersonGame.UI;
 using UnityEngine;
 
 namespace Assets.Scripts.ThirdPersonGame
 {
     public class EntryPoint : MonoBehaviour
     {
-        private void Start()
-        {
-            var uICreator = new UICreator();
-        }
+
 
     }
 }
